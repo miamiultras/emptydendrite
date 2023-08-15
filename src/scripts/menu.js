c665d77592d9b52e172cae139d0601c0679aafb1
eeeyyy <26793642+miamiultras@users.noreply.github.com>
2023-08-15 10:53:18 +0200

    refactor header and navbar, clean up global styles

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,10 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
+function toggleClasses() {
   document.querySelector('.nav-links').classList.toggle('expanded');
-});
+  document.querySelector('.hamburger').classList.toggle('hidden');
+  document.querySelector('.hamburger-close').classList.toggle('visible');
+  document.querySelector('html').classList.toggle('stuck');
+  document.querySelector('#themeToggle').classList.toggle('hidden');
+}
+
+document.querySelector('.hamburger').addEventListener('click', toggleClasses);
+document.querySelector('.hamburger-close').addEventListener('click', toggleClasses);
